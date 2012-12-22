366aca0dda8b61a34c1f7b447e94cbadced18e8b
bugcloud <bugcloudisco@bugcloud.com>
2012-12-23 00:55:09 +0900

    ユーザ名を設定できるようにテキストフィールドを追加

--- FrameCamera/SettingViewController.m
+++ FrameCamera/SettingViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SettingViewController.h"
+#import "Macros.h"
 
 @interface SettingViewController ()
 
@@ -14,9 +15,9 @@
 
 @implementation SettingViewController
 
-@synthesize delegate, keyForDateVisibleSetting_, keyForGridVisibleSetting_,
-valueForDateVisibleSetting_, valueForGridVisibleSetting_,
-switchForDateVisibleSetting_, switchForGridVisibleSetting_;
+@synthesize delegate, keyForDateVisibleSetting_, keyForGridVisibleSetting_, keyForUserNameSetting_,
+valueForDateVisibleSetting_, valueForGridVisibleSetting_, valueForUserNameSetting_,
+switchForDateVisibleSetting_, switchForGridVisibleSetting_, textFieldForNameSetting_;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -24,9 +25,12 @@ switchForDateVisibleSetting_, switchForGridVisibleSetting_;
     if (self) {
         self.keyForDateVisibleSetting_ = @"KEY_SETTING_DATE_VISIBLE";
         self.keyForGridVisibleSetting_ = @"KEY_SETTING_GRID_VISIBLE";
+        self.keyForUserNameSetting_ = @"KEY_SETTING_USER_NAME";
         NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
         self.valueForDateVisibleSetting_ = [defaults boolForKey:self.keyForDateVisibleSetting_];
         self.valueForGridVisibleSetting_ = [defaults boolForKey:self.keyForGridVisibleSetting_];
+        self.valueForUserNameSetting_ = [defaults stringForKey:self.keyForUserNameSetting_];
+        [self.textFieldForNameSetting_ setText:self.valueForUserNameSetting_];
     }
     return self;
 }
@@ -45,9 +49,13 @@ switchForDateVisibleSetting_, switchForGridVisibleSetting_;
 
 - (IBAction)saveSettings:(id)sender
 {
+    // Hide keyboard
+    [self textFieldShouldReturn:self.textFieldForNameSetting_];
+    
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     [defaults setBool:self.valueForDateVisibleSetting_ forKey:self.keyForDateVisibleSetting_];
     [defaults setBool:self.valueForGridVisibleSetting_ forKey:self.keyForGridVisibleSetting_];
+    [defaults setObject:self.valueForUserNameSetting_ forKey:self.keyForUserNameSetting_];
     [defaults synchronize];
     
     if (self.delegate) {
@@ -63,6 +71,16 @@ switchForDateVisibleSetting_, switchForGridVisibleSetting_;
 - (IBAction)didSwitchForGridVisibleSettingChanged:(id)sender
 {
     self.valueForGridVisibleSetting_ = ([sender isOn])? YES : NO;
+}
+
+
+#pragma mark -
+#pragma mark UITextFieldDelegate
+- (BOOL)textFieldShouldReturn:(UITextField *)theTextField
+{
+    self.valueForUserNameSetting_ = textFieldForNameSetting_.text;
+    [self.textFieldForNameSetting_ resignFirstResponder];
+    return YES;
 }
 
 @end
